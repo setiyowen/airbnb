4c2dc2197c026529e5da057063c64726fff226a1
WenTiger <nur.setiyowen@gmail.com>
2023-05-29 19:42:10 +0700

    huf

--- components/Heber.vue
+++ components/Heber.vue
@@ -1,21 +1,14 @@
 <template>
-  <div class="mt-20 ml-20 mb-24">
+  <div class="mt-[88px] ml-[80px]">
     <h1 class="font-semibold mt-[88] ml-[80] mb-[24] text-xl">
       Hébergements Airbnb Plus
     </h1>
-    <p class="font-semi">Hébergements Airbnb Plus</p>
-    <div class="flex mb-4">
-      <div class="w-full bg-white h-12">
-        <div>
-          <img src="/img/h1.png" alt="" />
-        </div>
-      </div>
+    <p class="font-semi mt-[8px]">
+      Une sélection de logements vérifiés selon des critères de qualité et de
+      design
+    </p>
+    <div class="mt-[24px]">
+      <img class="w-[1206px] t-[79px] h-[310px]" src="/img/h1.png" />
     </div>
   </div>
 </template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
--- components/deco.vue
+++ components/deco.vue
@@ -1,28 +1,60 @@
 <template>
-  <div class="mt-[300px] ml-[80px] mb-[24px] text-xl font-semibold">
-    <h1>Découvrez les aventures Airbnb</h1>
-    <p class="mt-100 ml-24 text-2xl font-medium">
+  <div class="mt-[88px] ml-[80px]">
+    <h1 class="font-semibold mt-[88] ml-[80] mb-[24] text-xl">
+      Découvrez les aventures Airbnb
+    </h1>
+    <p class="mt-[8px] font-semi">
       Voyages de plusieurs jours organisés par des experts locaux avec
       activités, repas et logements compris
     </p>
-    <div class="flex mb-4">
-      <div class="flex-1 h-12">
+    <div class="">
+      <div class="mt-[24px] grid lg:grid-cols-6 md:grid-cols-3">
         <img src="/img/deco1.png" class="h-auto" />
+        <p class="text-xs font-sm">Cayman Islands</p>
+        <p class="text-base font-semibold">2 Nights PACKAGE All Inclusive</p>
+        <p class="text-xs font-serif">
+          À partir de 577€/personne - Rp 800.000 / malam
+        </p>
       </div>
       <div class="flex-1 h-12">
         <img src="/img/deco2.png" class="h-auto" />
+        <p class="text-xs font-sm">Cayman Islands</p>
+        <p class="text-base font-semibold">2 Nights PACKAGE All Inclusive</p>
+        <p class="text-xs font-serif">
+          À partir de 577€/personne - Rp 800.000 / malam
+        </p>
       </div>
       <div class="flex-1 h-12">
         <img src="/img/deco3.png" class="h-auto" />
+        <p class="text-xs font-sm">Cayman Islands</p>
+        <p class="text-base font-semibold">2 Nights PACKAGE All Inclusive</p>
+        <p class="text-xs font-serif">
+          À partir de 577€/personne - Rp 800.000 / malam
+        </p>
       </div>
       <div class="flex-1 h-12">
         <img src="/img/deco4.png" class="h-auto" />
+        <p class="text-xs font-sm">Cayman Islands</p>
+        <p class="text-base font-semibold">2 Nights PACKAGE All Inclusive</p>
+        <p class="text-xs font-serif">
+          À partir de 577€/personne - Rp 800.000 / malam
+        </p>
       </div>
       <div class="flex-1 h-12">
         <img src="/img/deco5.png" class="h-auto" />
+        <p class="text-xs font-sm">Cayman Islands</p>
+        <p class="text-base font-semibold">2 Nights PACKAGE All Inclusive</p>
+        <p class="text-xs font-serif">
+          À partir de 577€/personne - Rp 800.000 / malam
+        </p>
       </div>
       <div class="flex-1 h-12">
         <img src="/img/deco6.png" class="h-auto" />
+        <p class="text-xs font-sm">Cayman Islands</p>
+        <p class="text-base font-semibold">2 Nights PACKAGE All Inclusive</p>
+        <p class="text-xs font-serif">
+          À partir de 577€/personne - Rp 800.000 / malam
+        </p>
       </div>
     </div>
   </div>
